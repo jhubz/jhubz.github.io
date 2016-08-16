9bbfd76f88d072ef808f551772c00112649ae667
jhubz <jhubz@list.ru>
2016-08-16 17:53:47 +0300

    test

--- js/formspree.js
+++ js/formspree.js
@@ -6,9 +6,11 @@ $contactForm.submit(function(e) {
 		method: 'POST',
 		data: $(this).serialize(),
 		dataType: 'json',
+        /*
 		beforeSend: function() {
 			$contactForm.append('<div class="alert alert-loading">Сообщение отправляется…</div>');
 		},
+        */
 		success: function(data) {
 			$contactForm.find('.alert--loading').hide();
 			$contactForm.append('<div class="alert alert-success">Сообщение отправлено!</div>');
