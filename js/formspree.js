39ec2a0be2483ccac1953dc2c503f482e39b07fd
jhubz <jhubz@list.ru>
2016-08-17 18:06:59 +0300

    form submit correction

--- js/formspree.js
+++ js/formspree.js
@@ -1,23 +1,35 @@
-var $contactForm = $('#contact-form');
-$contactForm.submit(function(e) {
+var contactForm = $('#contact-form');
+contactForm.submit(function(e) {
 	e.preventDefault();
 	$.ajax({
 		url: '//formspree.io/jhubz-work@list.ru',
 		method: 'POST',
 		data: $(this).serialize(),
 		dataType: 'json',
-        /*
+		
 		beforeSend: function() {
-			$contactForm.append('<div class="alert alert-loading">Сообщение отправляется…</div>');
+			contactForm.append('<div class="alert alert-loading">Сообщение отправляется…</div>');
 		},
-        */
+		
 		success: function(data) {
-			$contactForm.find('.alert--loading').hide();
-			$contactForm.append('<div class="alert alert-success">Сообщение отправлено!</div>');
+			contactForm.find('.alert-loading').hide();
+			contactForm.append('<div class="alert alert-success">Сообщение отправлено!</div>');
+			contactForm.trigger('reset');
+			
+			setTimeout( function () {
+				contactForm.find('.alert-success').hide();
+			}, 6000 );
+			
 		},
+		
 		error: function(err) {
-			$contactForm.find('.alert--loading').hide();
-			$contactForm.append('<div class="alert alert-error">Ошибка отправки сообщения.</div>');
+			contactForm.find('.alert-loading').hide();
+			contactForm.append('<div class="alert alert-error">Ошибка отправки сообщения.</div>');
+			
+			setTimeout( function () {
+				contactForm.find('.alert-error').hide();
+			}, 5000 );
+			
 		}
 	});
 });
